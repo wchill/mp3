3c60f80b6aa146a52f92ea5e358f9a4d784de894
Eric Ahn <wchill1337@gmail.com>
2016-03-09 05:53:18 +0000

    Done

--- source_js/app.js
+++ source_js/app.js
@@ -1,5 +1,24 @@
 var app = angular.module('mp3',['ngRoute']);
 
 app.config(function ($routeProvider) {
-
-})
+    $routeProvider
+        .when('/list', {
+            templateUrl : 'partials/list.html',
+            controller: 'movielist'
+        })
+        .when('/gallery', {
+            templateUrl : 'partials/gallery.html',
+            controller: 'moviegallery'
+        })
+        .when('/gallery/:genre', {
+            templateUrl : 'partials/gallery.html',
+            controller: 'moviegallery'
+        })
+        .when('/details/:id', {
+            templateUrl : 'partials/details.html',
+            controller: 'moviedetails'
+        })
+        .otherwise({
+            redirectTo: '/list'
+        });
+}) 
--- source_js/controllers.js
+++ source_js/controllers.js
@@ -1,1 +1,56 @@
 /* Sample Controller */
+app.controller('movielist', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
+    $http.get('./data/imdb250.json').success(function(data) {
+      $scope.movies = data;
+    }).error(function(err) {
+
+    });
+    $scope.query = "";
+    $scope.predicate = "title";
+    $scope.order = "ascending";
+    $scope.reverseSort = false;
+    $scope.setOrder = function(newOrder) {
+      $scope.order = newOrder;
+      $scope.reverseSort = ($scope.order === "descending");
+    }
+}]);
+app.controller('moviedetails', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
+    $http.get('./data/imdb250.json').success(function(data) {
+      for(var i = 0; i < data.length; i++) {
+        if(data[i] && data[i].imdbID === $routeParams.id) {
+          $scope.movie = data[i];
+          $scope.prev = data[(i + data.length - 1) % data.length].imdbID;
+          $scope.next = data[(i + 1) % data.length].imdbID;
+          break;
+        }
+      }
+    }).error(function(err) {
+
+    });
+}]);
+app.controller('moviegallery', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
+    $scope.genres = ["All", "Action", "Adventure", "Animation", "Biography", "Comedy", "Crime", "Drama", "Family", "Fantasy", "Film-Noir", "History", "Horror", "Music", "Musical", "Mystery", "Romance", "Sci-Fi", "Sport", "Thriller", "War", "Western"];
+    $scope.selectedGenre = "All";
+    $http.get('./data/imdb250.json').success(function(data) {
+      if($routeParams.genre && $routeParams.genre !== "All") {
+        $scope.movies = []; 
+        for(var i = 0; i < data.length; i++) {
+          if(data[i] && data[i].genre.indexOf($routeParams.genre) > -1) {
+            $scope.movies.push(data[i]);
+          }
+        }
+      } else {
+        $scope.movies = data;
+      }
+    }).error(function(err) {
+
+    });
+    $scope.filterGenre = function(movie) {
+      if($scope.selectedGenre && $scope.selectedGenre !== "All" && movie.genre.indexOf($scope.selectedGenre) == -1)
+        return false;
+      return true;
+    }
+    $scope.refreshList = function() {
+      $scope.$apply();
+    }
+}]);
